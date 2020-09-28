12d2e93cbb1bdeff120a5e50c604513c384d9b57
manu.sierra <manu.sierra@outlook.com>
2020-09-28 13:59:20 +0200

    Añadimos objeto saxo

--- index.php
+++ index.php
@@ -34,5 +34,41 @@ echo $piso -> vender();
 echo '<br>';
 echo $piso -> habitaciones=4;
 
+echo <br><br>;
+?>
+
+<?php 
+
+// Objeto saxofón
+// Declaramos clase.
+
+class Saxo {
+    //Declaramos atributos de saxo
+    public $baño;
+    public $tonalidad;
+    public $boquilla;
+    public $marca;
+
+    // Declarar métodos.
+    function comprar() {
+        echo 'Se ha comprado un saxo';
+        }
+    function vender() {
+        echo 'Se ha vendido un saxo';
+    }
+
+}
+
+//Instanciamos un objeto.
+
+$saxofon= new Saxo;
+
+$saxofon -> comprar();
+echo '<br>';
+echo 'con baño' . $saxofon -> baño=' dorado, afinado en' . $saxofon -> tonalidad=' Bb, con boquilla'.$saxofon -> boquilla=' metálica y de la marca' . $saxofon -> marca=' Tone King.';
+
+
+
+
 
 ?>
